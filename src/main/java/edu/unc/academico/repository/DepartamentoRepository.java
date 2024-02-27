9a4073f5517aa316bb411cf386549edeca6571f1
Peter Vásquez <pavs3013@gmail.com>
2024-02-27 11:01:48 -0500

    Primer comit - versión inicial del proyecto Academico

---
+++ src/main/java/edu/unc/academico/repository/DepartamentoRepository.java
@@ -0,0 +1,7 @@
+package edu.unc.academico.repository;
+
+
+import org.springframework.data.repository.CrudRepository;
+import edu.unc.academico.domain.Departamento;
+public interface DepartamentoRepository extends CrudRepository<Departamento, Long> {	
+}
